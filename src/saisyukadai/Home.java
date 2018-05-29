f17e2bf1463d8781ce4b6e6fe9ef5c60e975e472
kouki7315 <k.hamada@goalist.co.jp>
2018-05-29 16:57:42 +0900

    modify

--- src/saisyukadai/Home.java
+++ src/saisyukadai/Home.java
@@ -1,6 +1,7 @@
 package saisyukadai;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -10,6 +11,10 @@ import java.net.MalformedURLException;
 import java.net.ProtocolException;
 import java.net.URL;
 import java.net.URLEncoder;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.text.SimpleDateFormat;
 import java.util.Iterator;
 
 import twitter4j.Query;
@@ -20,40 +25,69 @@ import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
 
 public class Home {
-    public static void main(String[] args) throws TwitterException{
+    public static void main(String[] args) throws TwitterException, InterruptedException{
+        Path answer = Paths.get("C:/TechTraining/resources/answer.csv");
         Twitter twitter = new TwitterFactory().getInstance();
         Query query = new Query();
 
         // 検索ワードの設定
-        query.setQuery("ニンテンドースイッチ");
+        String word = "#ヨルナイト";
+        word = word + " -rt";
+        query.setQuery(word);
 
-        // 1度のリクエストで取得するTweetの数
-        query.setCount(20);
-
+        // 1度のリクエストで取得するTweetの数（最大100件）
+        query.setCount(100);
+        query.resultType(Query.RECENT);
         QueryResult result = null;
-        try {
-            result = twitter.search(query);
-        } catch (TwitterException e) {
-            e.printStackTrace();
-        }
-        Iterator itr = result.getTweets().iterator();
-        String senti = null;
 
-        while (itr.hasNext()) {
-            Status status = (Status)itr.next();
-            String text = status.getText();
-            Sentiment(text,senti);
-            if (!senti.equals("negative")) {
-                System.out.println(text); // 本文
-                System.out.println(status.getUser().getName()); // ユーザー名
-                System.out.println(status.getCreatedAt()); // 日付
-                System.out.println( );
+        try (BufferedWriter bw = Files.newBufferedWriter(answer)) {
+            bw.write("ユーザー名,日付,本文");
+            bw.newLine();
+
+            // 最大1500件（15ページ）
+            for (int i = 1; i <= 1; i++) {
+                try {
+                    result = twitter.search(query);
+                } catch (TwitterException e) {
+                    e.printStackTrace();
+                }
+                Iterator<?> itr = result.getTweets().iterator();
+                String senti = null;
+
+                while (itr.hasNext()) {
+                    Status status = (Status)itr.next();
+                    String text = status.getText();
+                    String senti2 = Sentiment(text,senti);
+                    if (!senti2.equals("negative")) {
+                        String name = status.getUser().getName();
+                        String date = new SimpleDateFormat("yyyy/MM/dd hh:mm:ss").format(status.getCreatedAt());
+                        text = text.replaceAll("\r", " ").replaceAll("\n", " ").replaceAll("\r/n", " ");
+                        System.out.println(text); // 本文
+                        System.out.println(name); // ユーザー名
+                        System.out.println(date); // 日付
+                        System.out.println( );
+                        String str = name + "," + date + "," + text;
+                        bw.write(str);
+                        bw.newLine();
+                    }
+                }
+
+                if (result.hasNext()) {
+                    query = result.nextQuery();
+                    Thread.sleep(1000);
+                } else {
+                    break;
+                }
             }
+
+            System.out.println("終了しました！");
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
 
-    public static void Sentiment (String text,String senti){
+    public static String Sentiment (String text,String senti){
 
         String urlstr = "https://api.apitore.com/api/11/sentiment/predict?access_token=5898cbe7-3c24-4624-b5d6-ae7850999728&text=";
         HttpURLConnection con = null;
@@ -94,6 +128,7 @@ public class Home {
             }
         }
 
+        //System.out.println(response);
         senti = response.substring(response.indexOf("sentiment") + 12,response.indexOf("sentiment") + 20);
 
         if (senti.contains("\""))
@@ -101,6 +136,7 @@ public class Home {
             senti = senti.replaceAll("\"", " ");
         }
         //System.out.println(senti);
+        return senti;
     }
 }
 
